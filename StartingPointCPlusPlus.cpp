a0433d344618d412a4673108882bb1d8b6296916
Juan Carlos Rodriguez Yarmas <jcry87@gmail.com>
2022-10-27 23:41:44 -0500

    Usage if, switch and while

--- StartingPointCPlusPlus.cpp
+++ StartingPointCPlusPlus.cpp
@@ -4,8 +4,45 @@ using namespace std;
 
 int main()
 {
+    // Conditionals
+    int number;
+    int reversedNumber = 0;
+    cout << "Please enter a number" << endl;
+    cin >> number;
+    (number % 2 == 0) ? cout << "You have entered even number " << endl : cout << "You have entered odd number" << endl;
+    // reversing number
+    while (number != 0)
+    {
+        reversedNumber *= 10;
+        reversedNumber += number % 10;
+        number /= 10;
+    }
+    cout << "Reversed number: " << reversedNumber << endl;
+    system("pause>0");
+    // operations
+    float num1, num2;
+    char operation;
+    cout << "Calculator" << endl;
+    cin >> num1 >> operation >> num2;
+    switch (operation)
+    {
+    case '+': cout << num1 << operation << num2 << "=" << num1 + num2; break;
+    case '-': cout << num1 << operation << num2 << "=" << num1 - num2; break;
+    case '*': cout << num1 << operation << num2 << "=" << num1 * num2; break;
+    case '/': cout << num1 << operation << num2 << "=" << num1 / num2; break;
+    }
+    // while
+    int counter = 100;
+    while (counter <= 500)
+    {
+        cout << counter << endl;
+        counter++;
+    }
+    system("pause>0");
+    //  I/O
     cout << "Helo! My name is Juan C Rodriguez" << endl;
     cout << "Please enter your annual salary" << endl;
+    // Variables
     float annualSalary;
     cin >> annualSalary;
     float monthlySalary = annualSalary / 12;
