f8a864d9579ff7958c256f51c0cf4e83bf5073e3
Juan Carlos Rodriguez Yarmas <jcry87@gmail.com>
2022-10-27 22:12:57 -0500

    Usage I/O and variables

--- StartingPointCPlusPlus.cpp
+++ StartingPointCPlusPlus.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Helo! My name is Juan C Rodriguez" << endl;
+    cout << "Please enter your annual salary" << endl;
+    float annualSalary;
+    cin >> annualSalary;
+    float monthlySalary = annualSalary / 12;
+    cout << "Your monthly salary is " << monthlySalary << endl;
+    char gender;
+    cout << "Please enter your gender [M,F]" << endl;
+    cin >> gender;
+    if (gender == 'M') {
+        cout << "Your gender is male" << endl;
+    }
+    else
+    {
+        cout << "Your gender is female" << endl;
+    }
+    system("pause>0");
 }
